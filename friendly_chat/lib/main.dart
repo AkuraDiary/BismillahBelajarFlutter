bb6b551cbcca4082e26ea734dcc25c72ce816731
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-10 00:06:09 +0700

    added chatScreen

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -10,17 +10,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return const FriendlyChatApp();
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friendly Chat'),
+      ),
+    );
+  }
+}
+
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Friendly Chat',
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Friendly Chat'),
-        ),
-      ),
+      home: const ChatScreen(),
     );
   }
 }
