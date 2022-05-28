accca311f86305f05b2ce3d739e624260e3d803c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 11:53:38 +0700

    added empty detail screen

--- codelab_wisata/lib/main.dart
+++ codelab_wisata/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:codelab_wisata/detail.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: DetailScreen(),
     );
   }
 }
