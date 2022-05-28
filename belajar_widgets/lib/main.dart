ba4c26c4ac15b9cd3b6c766fa88a23a3b931993a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 08:34:32 +0700

    added Heading stateleswidget and use it in the MyApp class

--- belajar_widgets/lib/main.dart
+++ belajar_widgets/lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp( MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Text("Hello world!"),
+          child: Heading(text: "Hello World!"),
         ),
       ),
     );
@@ -21,5 +23,18 @@ class MyApp extends StatelessWidget {
 }
 
 class Heading extends StatelessWidget{
+  final String text;
 
+  const Heading({required this.text});
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize : 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
 }
