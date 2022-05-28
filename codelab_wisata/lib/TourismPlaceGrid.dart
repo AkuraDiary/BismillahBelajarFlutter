548da27beebf18aed2c33e22305f2d0e0c01853e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 21:51:34 +0700

    added tourism place grid

---
+++ codelab_wisata/lib/TourismPlaceGrid.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TourismPlaceGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: [],
+      ),
+    );
+  }
+}
