d563eb222af70d5604e08c5b6e513531e4d16291
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 18:53:24 +0700

    added main_screen dart

--- codelab_wisata/lib/main.dart
+++ codelab_wisata/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:codelab_wisata/detail_screen.dart';
+import 'package:codelab_wisata/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
