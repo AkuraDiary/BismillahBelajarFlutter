ca3250adb7f4bb56fc1930952d51d19defb7570c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 13:56:17 +0700

    added judul in details screen

--- codelab_wisata/lib/detail.dart
+++ codelab_wisata/lib/detail.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class DetailScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container (
+              margin: EdgeInsets.only(top: 16.0),
+              child : Text(
+                  "Farm House Lembang",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
