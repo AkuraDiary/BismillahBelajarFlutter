51970ee0c45c05c6aeb965fe80f10d78bfe8c24c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 23:51:54 +0700

    added DetailWeb Page

---
+++ codelab_wisata/lib/DetailWebPage.dart
@@ -0,0 +1,107 @@
+import 'package:codelab_wisata/model/tourism_place.dart';
+import 'package:flutter/material.dart';
+
+class DetailWebPage extends StatelessWidget{
+  var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+  final TourismPlace place;
+
+  DetailWebPage({required this.place});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+              "Wisata bandung",
+            style: TextStyle(
+              fontSize: 32,
+              fontFamily: 'Staatliches',
+            ),
+          ),
+          SizedBox(height: 32),
+
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: Column(
+                    children: [],
+                  )
+              ),
+              SizedBox(width: 32),
+              Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              place.name,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontFamily: 'Staatliches',
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.calendar_today),
+                              SizedBox(width: 8.0),
+                              Text(
+                                place.openDays,
+                                style: informationTextStyle,
+                              ),
+                            ],
+                          ),
+
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.access_time),
+                              SizedBox(width: 8.0),
+                              Text(
+                                place.openTime,
+                                style: informationTextStyle,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8.0),
+
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.monetization_on),
+                              SizedBox(width: 8.0),
+                              Text(
+                                place.ticketPrice,
+                                style: informationTextStyle,
+                              ),
+                            ],
+                          ),
+
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            child: Text(
+                              place.description,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontFamily: 'Oxygen',
+                              ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- codelab_wisata/lib/detail_screen.dart
+++ codelab_wisata/lib/detail_screen.dart
@@ -6,105 +6,18 @@ class DetailScreen extends StatelessWidget {
 
   DetailScreen({required this.place});
 
-  var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                /*MAIN IMAGES*/
-                Image.asset('images/farm-house.jpg'),
-                SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: IconButton(
-                          icon: Icon(Icons.arrow_back, color: Colors.blue),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          }),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            /*JUDUL*/
-            Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: Text(
-                place.name,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Staatliches',
-                ),
-              ),
-            ),
-            /*FAVORITE BUTTON*/
-            FavoriteButton(),
-
-            /*ICONS*/
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(children: <Widget>[
-                    Icon(Icons.calendar_today),
-                    SizedBox(height: 8.0),
-                    Text(place.openDays, style: informationTextStyle),
-                  ]),
-                  Column(children: <Widget>[
-                    Icon(Icons.lock_clock_rounded),
-                    SizedBox(height: 8.0),
-                    Text(place.openTime, style: informationTextStyle),
-                  ]),
-                  Column(children: <Widget>[
-                    Icon(Icons.monetization_on_rounded),
-                    SizedBox(height: 8.0),
-                    Text(place.ticketPrice, style: informationTextStyle)
-                  ]),
-                ],
-              ),
-            ),
-
-            /*KONTENT DESKRIPSI*/
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                place.description,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-
-            /*IMAGE FROM NETWORK*/
-            Container(
-              height: 150.0,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: place.imageUrls.map((url) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(url),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints){
+        if(constraints.maxWidth > 800){
+          return DetailWebPage(place: place);
+        }else{
+          return DetailMobilePage(place: place);
+        }
+      },
     );
   }
 }
