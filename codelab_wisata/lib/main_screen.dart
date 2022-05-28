798952b77b49b53f560f7e1ce5cd7e3759c0e023
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 19:07:04 +0700

    wrapped the card into the inkwell

--- codelab_wisata/lib/main_screen.dart
+++ codelab_wisata/lib/main_screen.dart
@@ -9,38 +9,41 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Wisata bandung"),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            /*IMAGES*/
-            Expanded(
-              flex: 1,
-              child: Image.asset('images/farm-house.jpg'),
-            ),
-            Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Text(
-                        "Farm Hous Lembang",
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text("Lembang"),
-                    ],
-                  ),
-                )
-            ),
-          ],
+      body: InkWell(
+        onTap: (){},
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              /*IMAGES*/
+              Expanded(
+                flex: 1,
+                child: Image.asset('images/farm-house.jpg'),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Text(
+                          "Farm Hous Lembang",
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text("Lembang"),
+                      ],
+                    ),
+                  )
+              ),
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
