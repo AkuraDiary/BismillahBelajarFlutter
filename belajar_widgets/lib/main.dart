ae8b63d10feb8750708eabb715ae8cb5cdb5e1a2
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 08:56:18 +0700

    added bigger text stateful widget

--- belajar_widgets/lib/main.dart
+++ belajar_widgets/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -22,19 +23,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Heading extends StatelessWidget{
+class Heading extends StatelessWidget {
   final String text;
 
   const Heading({required this.text});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Text(
       text,
       style: const TextStyle(
-        fontSize : 24.0,
+        fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.text,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+            child: Text("Perbesar"),
+            onPressed: () {
+              setState(() {
+                _textSize = 32.0;
+              });
+            }
+            )
+      ],
     );
   }
 }
