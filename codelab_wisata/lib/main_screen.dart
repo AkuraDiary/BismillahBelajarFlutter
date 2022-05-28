dd55dc6488c59f96eb74f759a9ec781f2bdb49a0
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 21:58:19 +0700

    added if else to make it responsive

--- codelab_wisata/lib/main_screen.dart
+++ codelab_wisata/lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:codelab_wisata/TourismPlaceGrid.dart';
 import 'package:codelab_wisata/TourismPlaceList.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,15 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Wisata bandung. Size: ${MediaQuery.of(context).size.width}"),
       ),
-      body: TourismPlaceList(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints){
+          if(constraints.maxWidth <= 600){
+            return TourismPlaceList();
+          }else{
+            return TourismPlaceGrid();
+          }
+        }
+      )
     );
   }
 }
