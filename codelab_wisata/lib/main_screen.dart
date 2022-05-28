b4ebe5baa270cd2718278f120ef01c288f81aac7
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 21:05:43 +0700

    changed the data in list, agar berkaitan sama static data

--- codelab_wisata/lib/main_screen.dart
+++ codelab_wisata/lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:codelab_wisata/detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:codelab_wisata/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget {
   var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
@@ -10,45 +11,50 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Wisata bandung"),
       ),
-      body: InkWell(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder:(context){
-            return DetailScreen();
-          }));
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen();
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  /*IMAGES*/
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(place.imageAsset),
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(
+                              place.name,
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(place.location),
+                          ],
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          );
         },
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              /*IMAGES*/
-              Expanded(
-                flex: 1,
-                child: Image.asset('images/farm-house.jpg'),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Text(
-                          "Farm Hous Lembang",
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text("Lembang"),
-                      ],
-                    ),
-                  )
-              ),
-            ],
-          ),
-        ),
-      )
+        itemCount: tourismPlaceList.length,
+      ),
     );
   }
 }
