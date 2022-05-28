1faf6963e85373df1be19ac31437aefc6da53f16
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 21:21:41 +0700

    linkin the main and detail screen

--- codelab_wisata/lib/detail_screen.dart
+++ codelab_wisata/lib/detail_screen.dart
@@ -1,13 +1,15 @@
+import 'package:codelab_wisata/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
+  final TourismPlace place;
 
-
+  DetailScreen({required this.place});
+  var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-          /*
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
@@ -18,7 +20,7 @@ class DetailScreen extends StatelessWidget {
             Container(
               margin: EdgeInsets.only(top: 16.0),
               child: Text(
-                "Farm House Lembang",
+                place.name,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30.0,
@@ -38,17 +40,17 @@ class DetailScreen extends StatelessWidget {
                   Column(children: <Widget>[
                     Icon(Icons.calendar_today),
                     SizedBox(height: 8.0),
-                    Text("Open Everyday", style: informationTextStyle),
+                    Text(place.openDays, style: informationTextStyle),
                   ]),
                   Column(children: <Widget>[
                     Icon(Icons.lock_clock_rounded),
                     SizedBox(height: 8.0),
-                    Text("09:00 - 20:00", style: informationTextStyle),
+                    Text(place.openTime, style: informationTextStyle),
                   ]),
                   Column(children: <Widget>[
                     Icon(Icons.monetization_on_rounded),
                     SizedBox(height: 8.0),
-                    Text("RP 25.000", style: informationTextStyle)
+                    Text(place.ticketPrice, style: informationTextStyle)
                   ]),
 
                 ],
@@ -59,7 +61,7 @@ class DetailScreen extends StatelessWidget {
             Container(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                place.description,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 16.0),
               ),
@@ -70,26 +72,19 @@ class DetailScreen extends StatelessWidget {
               height: 150.0,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
+                children: place.imageUrls.map((url){
+                  return Padding(
                     padding: const EdgeInsets.all(4.0),
-                    child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
-                  )
-                ],
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
+                    ),
+                    );
+                }).toList(),
               ),
             ),
           ],
-        ),*/
+        ),
       ),
     );
   }
