733784b142a1d5641f58619d36137badd9ed2d17
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-28 22:07:35 +0700

    added responsive for web and mobile

--- codelab_wisata/lib/TourismPlaceGrid.dart
+++ codelab_wisata/lib/TourismPlaceGrid.dart
@@ -1,3 +1,5 @@
+import 'package:codelab_wisata/detail_screen.dart';
+import 'package:codelab_wisata/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 class TourismPlaceGrid extends StatelessWidget {
@@ -6,9 +8,47 @@ class TourismPlaceGrid extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(24.0),
       child: GridView.count(
-        crossAxisCount: 4,
-        children: [],
-      ),
+            crossAxisCount: 4,
+            children: tourismPlaceList.map((place) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DetailScreen(place: place);
+                  }));
+                },
+                child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Image.asset(
+                          place.imageAsset,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          place.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                        child: Text(
+                          place.location,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
     );
   }
 }
