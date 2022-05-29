c1995d2b86946bf27483e42216b2a396e40ff457
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 16:17:38 +0700

    added the tablet layout

---
+++ submission_pemula_flutter/lib/views/home/home_tablet.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import '../../models/books.dart';
+import '../layout/BooksGridLayout.dart';
+
+class HomeTabletLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 16, top: 20),
+                child: Text("Selamat Datang di Perpustakaan",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat",
+                        color: Colors.deepOrange
+                    )
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 16, right: 16, top: 20),
+              child: Text("Best Seller Today",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Montserrat",
+                      color: Colors.deepOrange
+                  )
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 8.0, left: 16, right: 8),
+              height: 450.0,
+              child: BooksGridLayout(
+                gridCount: 3,
+                scrollDirection: Axis.vertical,
+                catalouge: bookCatalougeList,
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+              child: Text("Rekomendasi lainya",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Montserrat",
+                      color: Colors.deepOrange
+                  )
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 8.0),
+              height: 250.0,
+              child: BooksGridLayout(
+                gridCount: 1,
+                scrollDirection: Axis.horizontal,
+                catalouge: bookCatalougeList.reversed,
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+              child: Text("Buku Terbaru",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Montserrat",
+                      color: Colors.deepOrange
+                  )
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 8.0),
+              height: 350.0,
+              child: BooksGridLayout(
+                gridCount: 1,
+                scrollDirection: Axis.vertical,
+                catalouge: bookCatalougeList.reversed,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
