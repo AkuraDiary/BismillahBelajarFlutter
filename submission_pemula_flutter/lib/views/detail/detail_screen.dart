a5b697fe6cf1a1c5f134f56255aba908be093810
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 14:39:40 +0700

    fookin around with these file

--- submission_pemula_flutter/lib/views/detail/detail_screen.dart
+++ submission_pemula_flutter/lib/views/detail/detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import '../../models/books.dart';
@@ -10,23 +12,39 @@ class DetailScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white,),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(bookItem.title),
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth > 800) {
-            return Text("TODO Layout Detail for Desktop"); //TODO DesktopDetailScreen(bookItem: bookItem);
-          } else {
-            return Text("TODO Layout Detail for Mobile"); //TODO MobileDetailScreen(bookItem: bookItem);
-          }
-        },
+      body: Container(
+          child: Column(
+            children: [
+              /*
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.blue,),
+                    onPressed: (){
+                      Navigator.pop(context);
+                      },
+                  ),
+                  Text(
+                    bookItem.title,
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+              */
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  if (constraints.maxWidth > 800) {
+                    return Text("TODO Layout Detail for Desktop"); //TODO DesktopDetailScreen(bookItem: bookItem);
+                  } else {
+                    return Text("TODO Layout Detail for Mobile"); //TODO MobileDetailScreen(bookItem: bookItem);
+                  }
+                },
+              ),
+            ],
+          ),
+
       ),
     );
   }
