e6698931c9448eeb2a405b3ac0d6a3899b0d6d4a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 21:31:47 +0700

    added the fab

--- submission_pemula_flutter/lib/views/main_screen.dart
+++ submission_pemula_flutter/lib/views/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:submission_pemula_flutter/views/button/floating_action_button.dart';
 
 import 'package:submission_pemula_flutter/views/home/home_desktop.dart';
 import 'package:submission_pemula_flutter/views/home/home_mobile.dart';
@@ -9,6 +10,7 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: SwitchThemeButton(),
       body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
             if (constraints.maxWidth <= 600) {
