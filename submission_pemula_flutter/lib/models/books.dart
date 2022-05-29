bd49bde5ab0be3907cffc7407c174e09d28d069f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 08:15:01 +0700

    added model class

---
+++ submission_pemula_flutter/lib/models/books.dart
@@ -0,0 +1,15 @@
+class Books {
+  String title;
+  String author;
+  String description;
+  String imageUrl;
+
+  Books({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.imageUrl
+  });
+}
+
+//TODO Var boook list
--- submission_pemula_flutter/test/widget_test.dart
+++ submission_pemula_flutter/test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:submission_pemula_flutter/app.dart';
 
 import 'package:submission_pemula_flutter/main.dart';
 
