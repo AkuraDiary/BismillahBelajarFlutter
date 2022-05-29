cd13a41927815ba9fa4d7264a6766ac50f5c1959
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 22:03:27 +0700

    added more books data

--- submission_pemula_flutter/lib/models/books.dart
+++ submission_pemula_flutter/lib/models/books.dart
@@ -87,6 +87,47 @@ var bookCatalougeList = [
       imageUrl: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1641215134l/60016154._SX318_.jpg"
   ),
 
+  Books(
+      title: 'Home Sweet Loan',
+      author: "Almira Bastari",
+      type: "Novel",
+      pages: "312",
+      date: "2022",
+      publisher: "GRAMEDIA PUSTAKA UTAMA",
+      description: "Empat orang yang berteman sejak SMA bekerja di perusahaan yang sama meski beda nasib. Di usia 31 tahun, mereka berburu rumah idaman yang minimal… nyerempet Jakarta. Kaluna, pegawai Bagian Umum, yang gajinya tak pernah menyentuh dua digit. Gadis ini kerja sampingan sebagai model bibir, bermimpi membeli rumah demi keluar dari situasi tiga kepala keluarga yang bertumpuk di bawah satu atap. Di tengah perjuangannya menabung, Kaluna dirongrong oleh kekasihnya untuk pesta pernikahan mewah. ",
+      imageUrl: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1646470594l/60129536._SX318_.jpg"
+  ),
 
+  Books(
+      title: 'Bibi Gill',
+      author: "Tere Liye",
+      type: "Novel",
+      pages: "356",
+      date: "2022",
+      publisher: "PENERBIT SABAK GRIP",
+      description: "Bagaimana menjadi petarung terkuat di dunia paralel? Dengan latihan panjang dan pengorbanan. Termasuk kehilangan dan kesedihan. Inilah kisah tentang Bibi Gill, yang sejak kecil berusaha mengalahkan 'monster' dalam hidupnya. Apakah dia bisa menemukan jawaban yang selama ini dia cari? Hei, jika kalian melihat seseorang yang amat sederhana, seperti ibu-ibu penjaga kantin, tukang sapu, sopir ojek online. Jangan buru-buru menilainya memang sesederhana itu. Boleh jadi dia adalah pemilik teknik bertarung paling mematikan di dunia paralel.",
+      imageUrl: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/2/3/ce8fccb2-300a-4684-bc41-84aa03a96907.jpg"
+  ),
 
+  Books(
+      title: 'Pagi di Amerika',
+      author: "Hikmat Darmawan",
+      type: "Novel",
+      pages: "181",
+      date: "2015",
+      publisher: "Serambi Ilmu Semesta",
+      description: "Ketika peluru memasuki jantungnya, gadis itu bersandar ke sebuah loker dan menyaksikan Kyle menembakkan pistol itu lagi dan lagi. Ketika gadis itu meluncur perlahan ke lantai, gadis itu mendapatkan pencerahan seakan sebuah vista, seluruh dunia terpandang dari sebuah puncak gunung. Surga dan dunia terkandung dalam sejarah pikiran : kami sungguh tak cukup mengasihi kyle, dan kyle telah menderita karena nya.",
+      imageUrl: "https://ebooks.gramedia.com/ebook-covers/24322/big_covers/ID_SIS2015MTH06PDAM_B.jpg"
+  ),
+
+  Books(
+      title: 'Yazid bin Abdul Qadir Jawas',
+      author: "Dzikir PAGI PETANG dan Sesudah Shalat",
+      type: "Religi",
+      pages: "126",
+      date: "2017",
+      publisher: "Pustaka Imam Asy-syafii",
+      description: "Allah memerintahkan kita untuk banyak berdzikir ketika pagi dan petang. Demikian pula ketika selesai mengerjakan shalat Fardhu, kita juga disyari'atkan untuk berdzikir kepada-Nya juga. Lalu bagaimanakah dan apa saja dzikir-dzikir yang telah diajarkan oleh Rasulullah? Dzikir-dzikir yang dicantumkan di dalam buku ini bersumber dari Rasulullah karena disebutkan di dalam kita-kitab hadits yang shahih. Jika diamailkan sungguh bermanfaat karena selain berdzikir kepada Allah, Anda juga turut menghidupkan Sunnah Rasulullah dalam berdzikir di saat pagi, petang, dan sesudah shalat fardhu.",
+      imageUrl: "https://cf.shopee.co.id/file/9c011d3cdc6081ea77cf381c92e83a9a"
+  ),
 ];
