4aa6283effb49f4d45154ad38c7cb1b46d497b65
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 10:43:34 +0700

    updated image url

--- submission_pemula_flutter/lib/models/books.dart
+++ submission_pemula_flutter/lib/models/books.dart
@@ -29,7 +29,7 @@ var bookCatalougeList = [
     date: "2017",
     publisher: "Kepustakaan Populer Gramedia",
     description: "Laut Bercerita, novel terbaru Leila S. Chudori, bertutur tentang kisah keluarga yang kehilangan, sekumpulan sahabat yang merasakan kekosongan di dada, sekelompok orang yang gemar menyiksa dan lancar berkhianat, sejumlah keluarga yang mencari kejelasan makam anaknya, dan tentang cinta yang tak akan luntur.",
-    imageUrl: "https://cdn.gramedia.com/uploads/items/9786024246945_Laut-Bercerita.jpg"
+    imageUrl: "https://cf.shopee.co.id/file/a6dee66656b54901ba52740847b8b17c"
   ),
 
   Books(
@@ -40,7 +40,7 @@ var bookCatalougeList = [
       date: "2022",
       publisher: "PENERBIT SABAK GRIP",
       description: "Akhirnya. Siapa orang tua Ali dijawab di buku ini. Ali, bertahun-tahun, berusaha memecahkan misteri itu. Paib dan Seli tentu tidak akan membiarkan Ali sendirian, mereka sahabat sejati. Dan jangan lupakan, Batozar alias Master B, dengan segenap kalimat kasar, seolah tidak peduli, dia selalu siap membela. Tapi bagaimana jika misteri itu terhadang tembok kokoh SagaraSe? Dan mereka harus bertarung hidup-mati lima ronde melawan Ksatria SagaraS?",
-      imageUrl: "https://cdn.gramedia.com/uploads/items/sagaras.jpeg"
+      imageUrl: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/4/15/17f74419-ce63-450e-80ff-674da1a2e329.jpg"
   ),
 
   Books(
@@ -51,7 +51,7 @@ var bookCatalougeList = [
       date: "2020",
       publisher: "Gramedia Widiasarana Indonesia",
       description: "Listrik padam di seluruh Jawa dan Bali secara misterius! Ancaman nyata kekuatan baru yang hendak menaklukkan Nusantara.Saat yang sama, empat sahabat mendarat di Sumba, hanya untuk mendapati nasib ratusan juta manusia ada di tangan mereka! Empat mahasiswa ekonomi ini, harus bertarung melawan pasukan berkuda yang bisa melontarkan listrik!",
-      imageUrl: "https://cdn.gramedia.com/uploads/items/9786020523316_Melangkah_UV_Spot_R4-1.jpg"
+      imageUrl: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1584173524l/52366756._SY475_.jpg"
   ),
 
 ];
