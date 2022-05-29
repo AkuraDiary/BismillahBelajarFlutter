67eb2ffe2f2c8f1002f4f7d7cb036428bb57b483
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-29 08:22:34 +0700

    added more data

--- submission_pemula_flutter/lib/models/books.dart
+++ submission_pemula_flutter/lib/models/books.dart
@@ -1,12 +1,16 @@
 class Books {
   String title;
   String author;
+  String type;
+  String pages;
   String description;
   String imageUrl;
 
   Books({
     required this.title,
     required this.author,
+    required this.type,
+    required this.pages,
     required this.description,
     required this.imageUrl
   });
